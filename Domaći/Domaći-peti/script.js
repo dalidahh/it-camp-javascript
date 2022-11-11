8c7912845b89b856510f68a8808f230684281567
Dalida <dalidah18@gmail.com>
2022-11-11 18:05:32 +0100

    done.

--- Domaći/Domaći-peti/script.js
+++ Domaći/Domaći-peti/script.js
@@ -11,7 +11,7 @@ Naci broj slova iz datog teksta (loremIpsumText)
 // if(slovo !== '' && slovo !== ' ' && slovo !== '.' && slovo !== ',')
 // 3. Izbrojati koliko taj dobijeni niz ima elemenata
 
-var loremIpsumText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book! It has survived not only five centuries, but also the leap into electronic typesetting? Remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+var loremIpsumText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book! It has survived not only five centuries, but also the leap into electronic typesetting remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
 var recenice = loremIpsumText.split(".");
 var rijeci = loremIpsumText.split(" ");
 var charactersInText = [];
@@ -20,7 +20,7 @@ console.log ("Rečenice", recenice);
 console.log ("Riječi", rijeci);
 
 for( var i = 0; i <= loremIpsumText.length -1; i++ ){
-    if( loremIpsumText[i]!=="." && loremIpsumText[i]!=="," && loremIpsumText[i]!=="!" && loremIpsumText[i]!=="?" && loremIpsumText[i]!==" " && loremIpsumText[i]!=="'"){
+    if( loremIpsumText[i]!=="." && loremIpsumText[i]!=="," && loremIpsumText[i]!=="!" && loremIpsumText[i]!==" " && loremIpsumText[i]!=="'"){
         charactersInText.push(loremIpsumText[i])
     }
   }
