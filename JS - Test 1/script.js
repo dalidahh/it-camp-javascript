a64960a223075a97e8dd058d50bdcc625057281f
Dalida <dalidah18@gmail.com>
2022-11-13 22:49:22 +0100

    Corrected.

--- JS - Test 1/script.js
+++ JS - Test 1/script.js
@@ -3,20 +3,6 @@ var k = 5;
 var numberOfPairs = 0;
 var indeksi = [];
 
-// for( var i = 0; i <= ar.length -2; i++) {
-//     for( var j = 1; j <= ar.length -1; j++) {
-//         if( i < j && ((ar[i] + ar[j]) % k) === 0 ) {
-//             if( !indeksi.includes(ar[i]) && !indeksi.includes(ar[j]) ) {
-//               indeksi.push(ar[i]);
-//               indeksi.push(ar[j]);
-//               numberOfPairs++;   
-//             }
-//         }
-//     }
-// }
-
-// console.log(indeksi); 
-
  for( var i = 0; i <= ar.length -2; i++) {
      for( var j = 0; j <= ar.length -1; j++) {
         if((ar[i] + ar[j]) % k  === 0 ) {
