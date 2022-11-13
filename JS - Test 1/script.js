74d0be78b24f48f82fd9025fb41958a8801e173a
Dalida <dalidah18@gmail.com>
2022-11-13 21:37:20 +0100

    Done.

---
+++ JS - Test 1/script.js
@@ -0,0 +1,18 @@
+var ar = [1, 2, 3, 4, 5, 6];
+var k = 5
+var numberOfPairs = 0;
+var indeksi = [];
+
+for( var i = 0; i <= ar.length -2; i++) {
+    for( var j = 1; j <= ar.length -1; j++) {
+        if( i < j && (ar[i] + ar[j]) % k === 0 ) {
+            if( !indeksi.includes(ar[i]) && !indeksi.includes(ar[j]) ) {
+              indeksi.push(ar[i]);
+              indeksi.push(ar[j]);
+              numberOfPairs++;
+            }
+        }
+    }
+}
+
+console.log(indeksi);
