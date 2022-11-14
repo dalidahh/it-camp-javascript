3299c63b8a7f11b37f126f1a253b2697b1adc3d4
Dalida <dalidah18@gmail.com>
2022-11-14 20:20:15 +0100

    Done.

--- JS - Test 1/script.js
+++ JS - Test 1/script.js
@@ -1,18 +1,43 @@
-var ar = [1, 2, 3, 4, 5, 6];
-var k = 5;
-var numberOfPairs = 0;
-var indeksi = [];
+// var ar = [1, 2, 3, 4, 5, 6];
+// var k = 5;
+// var numberOfPairs = 0;
+// var indeksi = [];
 
- for( var i = 0; i <= ar.length -2; i++) {
-     for( var j = 1; j <= ar.length -1; j++) {
-        if((ar[i] + ar[j]) % k  === 0 ) {
-            if( i < j ) {
-               indeksi.push(ar[i]);
-               indeksi.push(ar[j]);
-               numberOfPairs++;
-            }     
-        }
-    }
-}     
+//  for( var i = 0; i <= ar.length -2; i++) {
+//      for( var j = 1; j <= ar.length -1; j++) {
+//         if((ar[i] + ar[j]) % k  === 0 ) {
+//             if( i < j ) {
+//                indeksi.push(ar[i]);
+//                indeksi.push(ar[j]);
+//                numberOfPairs++;
+//             }     
+//         }
+//     }
+// }     
 
- console.log(indeksi); 
+//  console.log(indeksi);
+
+// Deliver the solution in this format
+
+// ar - array of numbers
+// n - length of ar
+// k - ar[i]+ar[j] should be devided by this number
+ 
+function divisibleSumPairs( ar, k) {
+    var numberOfPairs = 0;
+     
+    for( var i = 0; i <= ar.length -2; i++) {
+             for( var j = 1; j <= ar.length -1; j++) {
+                if((ar[i] + ar[j]) % k === 0 ) {
+                    if( i < j ) {
+                    numberOfPairs++;
+                    }     
+                }
+            }
+        }     
+     
+        return numberOfPairs;
+}
+
+var result = divisibleSumPairs([1, 2, 3, 4, 5, 6], 5 );
+console.log(result)
